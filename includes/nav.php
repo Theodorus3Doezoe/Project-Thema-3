0197a8bc8541876bf4f2796005c23e844c2d39ca
theodorus3doezoe <svendinkla@gmail.com>
2022-03-29 11:08:54 +0200

    standaard styling

---
+++ includes/nav.php
@@ -0,0 +1,8 @@
+<ul>
+    <img src="images/logo.svg" alt="logo">
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul>
--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KW1C</title>
+    <title>Koning Willem 1 College</title>
+    <link rel="icon" href="images/favicon-32x32.png">
+    <link rel="stylesheet" href="styles/styles-algemeen.css">
 </head>
 <body>
-    <?php include 'includes/nav';?>
+    <?php include 'includes/nav.php';?>
 </body>
 </html>
