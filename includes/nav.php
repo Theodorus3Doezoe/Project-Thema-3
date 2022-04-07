057575f0160c8b90f601c8fcb6b2b4f4efe8c11a
rickyseverins2005 <ricky.severins@edu-kw1c.nl>
2022-04-07 15:10:31 +0200

    homepage

--- includes/nav.php
+++ includes/nav.php
@@ -1,19 +1,46 @@
-<div id="navbc">
-    <ul id="ulnav">
-        <div>
-        <img id="logo" src="../images/logo.svg" alt="logo">
-        </div>
-        <div>
-        <li class="nav" id="nav"><a class="anav" href="#">opleidingen</a></li>
-        </div>
-        <div>
-        <li class="nav"><a class="anav" href="#">studiekeuze</a></li>
-        </div>
-        <div>
-        <li class="nav"><a class="anav" href="#">volwassenen & werkgevers</a></li>
-        </div>
-        <div>
-        <li class="nav"><a class="anav" href="#">studenteninfo</a></li>
-        </div>
-    </ul>
+<div id="header">
+    <div id="navbc">
+        <ul id="ulnav">
+            <div>
+                <img id="logo" src="../images/logo.svg" alt="logo">
+            </div>
+            <div>
+                <li class="nav" id="nav"><a class="anav" href="#">opleidingen</a></li>
+            </div>
+            <div>
+                <li class="nav"><a class="anav" href="#">studiekeuze</a></li>
+            </div>
+            <div>
+                <li class="nav"><a class="anav" href="#">volwassenen & werkgevers</a></li>
+            </div>
+            <div>
+                <li class="nav"><a class="anav" href="#">studenteninfo</a></li>
+            </div>
+            <div class="menuheader">
+                <a href="#">
+                    <div>
+                        <img class="menuheader" id="vlag" src="../images/vlag.png" alt="logo">
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <img class="menuheader" id="portaal" src="../images/portaal.png" alt="logo">
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <img class="menuheader" id="zoeken" src="../images/zoeken.png" alt="logo">
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <img class="menuheader" id="menu" src="../images/menu.png" alt="logo">
+                    </div>
+                </a>
+            </div>
+        </ul>
+    </div>
+</div>
+<div id="pzelftest">
+    <a id="zelftestknop" href="zelftest.php">zelftest</a>
 </div>
--- pages/index.php
+++ pages/index.php
@@ -10,55 +10,51 @@
     <link rel="stylesheet" href="../styles/styles-algemeen.css">
 </head>
 <body>
-    <div id="header">
-        <header>
-            <!--nav via include-->
-            <?php include '../includes/nav.php';?>
-        </header>
+<?php include '../includes/nav.php';?>
+    <div id="banner">
     </div>
-    <main id="banner">
-        <p id="pzelftest">
-            <a id="zelftestknop" href="zelftest.php">zelftest</a>
-        </p>
+    <main>
+
+        <img id="stippelijn" src="../images/gestippelde%20lijn.png" alt="stippellijn">
+        <h1>
+            ICT-Acedemie
+        </h1>
+        <div id="block1">
+            <h3>
+                algemeen
+            </h3>
+            <p>
+                Een goede ICT’er is gedreven om te leren en is bereid om kennis te delen met anderen. Dat vinden ze bij het bedrijfsleven. Verder moet je wat willen bereiken, plezier hebben in wat je doet en goed in een team kunnen werken. Tijdens je studie aan de ICT-Academie krijg je hiermee te maken. Dat gebeurt zoveel mogelijk in projectvormen: leren werken en werkend leren in klein groepsverband, met opdrachten uit de praktijk. Teamwork dus. Belangrijk is verder de adviesraad, die bestaat uit vertegenwoordigers van vooraanstaande bedrijven uit de regio.
+            </p>
+        </div>
+        <div id="block2">
+            <h3>
+                informatie
+            </h3>
+            <p>
+                Download de brochure van deze afdeling:
+            </p>
+            <p id="brochure">
+                <a id="azelftest" href="#">
+                    brochure in pdf
+                </a>
+            </p>
+        </div>
+        <div id="block3">
+            <h3>
+                contact
+            </h3>
+            <p>
+                tel: 073 624 9 600
+            </p>
+            <p>
+                email: info@kw1c.nl
+            </p>
+        </div>
     </main>
-    <h1>
-        ICT-Acedemie
-    </h1>
     <img id="stippelijn" src="../images/gestippelde%20lijn.png" alt="stippellijn">
-    <footer>
-        <?php include '../includes/footer.php';?>
-    </footer>
-    <div id="block1">
-        <h3>
-            algemeen
-        </h3>
-        <p>
-            Een goede ICT’er is gedreven om te leren en is bereid om kennis te delen met anderen. Dat vinden ze bij het bedrijfsleven. Verder moet je wat willen bereiken, plezier hebben in wat je doet en goed in een team kunnen werken. Tijdens je studie aan de ICT-Academie krijg je hiermee te maken. Dat gebeurt zoveel mogelijk in projectvormen: leren werken en werkend leren in klein groepsverband, met opdrachten uit de praktijk. Teamwork dus. Belangrijk is verder de adviesraad, die bestaat uit vertegenwoordigers van vooraanstaande bedrijven uit de regio.
-        </p>
-    </div>
-    <div id="block2">
-        <h3>
-            informatie
-        </h3>
-        <p>
-            Download de brochure van deze afdeling:
-        </p>
-        <p id="brochure">
-            <a id="azelftest" href="#">
-                brochure in pdf
-            </a>
-        </p>
-    </div>
-    <div id="block3">
-        <h3>
-            contact
-        </h3>
-        <p>
-           tel: 073 624 9 600
-        </p>
-        <p>
-           email: info@kw1c.nl
-        </p>
-    </div>
+    <?php
+    include '../includes/footer.php';
+    ?>
 </body>
 </html>
