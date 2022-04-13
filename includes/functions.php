8384de9ecf54dd446d9912aee840a5cc39712476
rickyseverins2005 <ricky.severins@edu-kw1c.nl>
2022-04-13 15:06:53 +0200

    homepage

--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,7 @@
 <?php
 function addpoints()
 {
-    $points = 0;
+    $points = 100;
     if (isset($_POST['q1']) == "ja")
     {
         $points = $points + 20;
@@ -205,19 +205,144 @@ function addpoints()
     {
         $points = $points - 5;
     }
-    if ($points < 60)
+    if ($points < 80)
     {
-        echo "<h2>Wij denken dat een ICT opleiding niet bij jou past</h2>";
-        echo "<img src='../images/niksvoorjou.jpg' alt='niksvoorjou'>";
+        echo "<h2>Wij denken dat een ICT opleiding niet bij jou past.</h2>";
+        echo "<img class='imgresults' src='../images/niksvoorjou.jpg' alt='niksvoorjou'>";
     }
-    elseif ($points > 60 && $points < 100)
+    elseif ($points > 80 && $points < 120)
     {
-        echo "<h2>Wij denken dat Software development een opleiding voor jou</h2>";
-        echo "<img src='../images/softwaredeveloper.webp' alt='niksvoorjou'>";
+        echo "<h2>Wij denken dat Software development een opleiding voor jou is.</h2>";
+        echo "<img class='imgresults' src='../images/softwaredeveloper.webp' alt='niksvoorjou'>";
     }
     else
     {
-        echo "<h2>Wij denken dat Expert IT Systems and Devices een opleiding voor jou is</h2>";
-        echo "<img src='../images/systeembeheer.jpg' alt=''>";
+        echo "<h2>Wij denken dat Expert IT Systems and Devices een opleiding voor jou is.</h2>";
+        echo "<img class='imgresults' src='../images/systeembeheer.jpg' alt=''>";
+    }
+    function showanswers()
+    {
+        echo "<h2>jouw antwoorden</h2>";
+        if (isset($_GET['q1']) == true)
+        {
+            echo "<h3>Vraag 1 Ben jij geïnteresseerd in technologie?</h3>";
+            echo $_GET['q1'];
+        }
+        else
+        {
+            echo "<h3>Vraag 1 Ben jij geïnteresseerd in technologie?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q2']) == true)
+        {
+            echo "<h3>Vraag 2 Heb je al eerdere ervaring opgedaan met ICT en vond je dit leuk?</h3>";
+            echo $_GET['q2'];
+        }
+        else
+        {
+            echo "<h3>Vraag 2 Heb je al eerdere ervaring opgedaan met ICT en vond je dit leuk?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q3']) == true)
+        {
+            echo "<h3>Vraag 3 Lijkt programmeren je interessant?</h3>";
+            echo $_GET['q3'];
+        }
+        else
+        {
+            echo "<h3>Vraag 3 Lijkt programmeren je interessant?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q4']) == true)
+        {
+            echo "<h3>Vraag 4  Zie jij jezelf later grotendeels achter je computer werken?</h3>";
+            echo $_GET['q4'];
+        }
+        else
+        {
+            echo "<h3>Vraag 4  Zie jij jezelf later grotendeels achter je computer werken?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q5']) == true)
+        {
+            echo "<h3>Vraag 5 Ben je goed in problemen oplossen?</h3>";
+            echo $_GET['q5'];
+        }
+        else
+        {
+            echo "<h3>Vraag 5 Ben je goed in problemen oplossen?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q6']) == true)
+        {
+            echo "<h3>Vraag 6 Raak je snel gefrustreerd?</h3>";
+            echo $_GET['q6'];
+        }
+        else
+        {
+            echo "<h3>Vraag 6 Raak je snel gefrustreerd?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q7']) == true)
+        {
+            echo "<h3>Vraag 7 Sleutel je graag aan computers?</h3>";
+            echo $_GET['q7'];
+        }
+        else
+        {
+            echo "<h3>Vraag 7 Sleutel je graag aan computers?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q8']) == true)
+        {
+            echo "<h3>Vraag 8 Lijkt het je leuk systemen te beheren?</h3>";
+            echo $_GET['q8'];
+        }
+        else
+        {
+            echo "<h3>Vraag 8 Lijkt het je leuk systemen te beheren?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q9']) == true)
+        {
+            echo "<h3>Vraag 9 Lijkt het je leuk netwerken op te zetten voor bijvoorbeeld een bedrijf?</h3>";
+            echo $_GET['q9'];
+        }
+        else
+        {
+            echo "<h3>Vraag 9 Lijkt het je leuk netwerken op te zetten voor bijvoorbeeld een bedrijf?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q10']) == true)
+        {
+            echo "<h3>Vraag 10 Ben je bereid door te blijven leren om altijd op de hoogte te zijn van de nieuwste technieken en technologische ontwikkelingen die belangrijk kunnen zijn voor jouw beroep?</h3>";
+            echo $_GET['q10'];
+        }
+        else
+        {
+            echo "<h3>Vraag 10 Ben je bereid door te blijven leren om altijd op de hoogte te zijn van de nieuwste technieken en technologische ontwikkelingen die belangrijk kunnen zijn voor jouw beroep?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q11']) == true)
+        {
+            echo "<h3>Vraag 11 Lijkt het beveiligen van netwerken je leuk?</h3>";
+            echo $_GET['q11'];
+        }
+        else
+        {
+            echo "<h3>Vraag 11 Lijkt het beveiligen van netwerken je leuk?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+        if (isset($_GET['q12']) == true)
+        {
+            echo "<h3>Vraag 12 Lijkt het je leuk mensen binnen een bedrijf/organisatie te helpen als hun problemen hebben met hun apparaten?</h3>";
+            echo $_GET['q12'];
+        }
+        else
+        {
+            echo "<h3>Vraag 12 Lijkt het je leuk mensen binnen een bedrijf/organisatie te helpen als hun problemen hebben met hun apparaten?</h3>";
+            echo "je hebt deze vraag niet ingevuld";
+        }
+
     }
 }
